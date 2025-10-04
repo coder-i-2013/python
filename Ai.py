7d3c561cbb8a7f0f6ed4adb6d04d4c332e7e1d5e
coder-i-2013 <ahanajanvi@gmail.com>
2025-10-04 09:10:30 -0400

    Create Ai.py

---
+++ Ai.py
@@ -0,0 +1,16 @@
+print("Hello, I am an Ai bot what is your name?")
+name= input("Your name: ")
+print("Nice to meet you ",name)
+
+print("How are you feeling today?")
+mood=input().lower()
+if mood=="good":
+    print("I am glad to hear that")
+elif mood=="bad":
+    print("Sorry to hear that, hope you feel better.")
+else:
+    print("Somtimes it is hard to share your feelings.")
+
+
+print("It was nice to chat with you",name,"goodbye.")
+
